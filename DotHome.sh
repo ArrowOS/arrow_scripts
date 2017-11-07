1322a76f591139c0ae5dfb00473825cebc56c1ff
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-11-07 19:31:53 +0000

    stop breaking my repo-sync thankyou
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- DotHome.sh
+++ DotHome.sh
@@ -2,14 +2,15 @@ echo -n "Do you want to include Dot launcher in this build ?(y/n) "
 echo
 echo -n "========================================="
 echo
-echo -n "By default Launchair will be installed"
+echo -n "By default Lawnchair will be installed"
 echo
 echo -n "=========================================="
 echo
 read -t 20 answer
 if echo "$answer" | grep -iq "^y" ;then
-	  echo Dot launcher will be installed alongside launchair
+	  echo Dot launcher will be installed alongside lawnchair
+	  export WITH_DOTHOME=true
 else
-    echo Dot launcher will not be installed alongside launchair
-	 sed -i '/Trebuchet/c\Lawnchair' vendor/dot/config/common.mk
+    echo Dot launcher will not be installed alongside lawnchair
+	  export WITH_DOTHOME=false
 fi
