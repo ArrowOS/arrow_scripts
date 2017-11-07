48ddd69d3aec1ffdcd17a944a45a038b057de03b
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-11-08 05:16:14 +0530

    lawnchair check
    when lawnchair is not found make DotHome default.
    
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- DotHome.sh
+++ DotHome.sh
@@ -6,11 +6,26 @@ echo -n "By default Lawnchair will be installed"
 echo
 echo -n "=========================================="
 echo
-read -t 20 answer
+
+CUR_DIR=`pwd`
+DOWN_PATH="$CUR_DIR/dot_scripts/Lawnchair"
+
+read -t 10 answer
 if echo "$answer" | grep -iq "^y" ;then
-	  echo Dot launcher will be installed alongside lawnchair
-	  export WITH_DOTHOME=true
+	if [[ -f $DOWN_PATH/Lawnchair.apk ]] ; then
+	    echo Dot launcher will be installed alongside lawnchair
+	    export WITH_DOTHOME=true
+	else
+		echo Lawnchair not found, only DotHome will be installed.
+		export WITH_DOTHOME=true
+	fi
 else
     echo Dot launcher will not be installed alongside lawnchair
 	  export WITH_DOTHOME=false
+
+	  #Lawnchair check
+	  if [[ ! -e $DOWN_PATH/Lawnchair.apk ]] ; then
+	  	echo No alternative Launcher found. DotHome will be included by default.
+	  	export WITH_DOTHOME=true
+	  fi
 fi
